360be4c0862f4eb5b05fb71fc0e3f55260418766
Caio Gomes <caiogomesteodoro21@gmail.com>
2023-06-26 21:12:19 -0300

    desativando scroll durante a sideBar

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var btnBurger = document.querySelector(".lineBurger");
 var btnClose = document.querySelector(".closeX");
 var sideBar = document.querySelector(".sideBar");
+var body = document.querySelector(".body");
+
 btnBurger.addEventListener("click", () => {
+  body.style.overflow = "hidden";
   sideBar.style.transition = "0.3s";
   sideBar.style.width = "100vw";
 });
 btnClose.addEventListener("click", () => {
   sideBar.style.transition = "0.3s";
   sideBar.style.width = "0vw";
+  body.style.overflow = "visible";
 });
