2ec86f8a5f2df79213b29720bd5a5396e14fba1e
Caio Gomes <caiogomesteodoro21@gmail.com>
2023-06-28 23:59:00 -0300

    adicionando funçao contatos no side bar / responsividade na caixa de texto 2

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var btnBurger = document.querySelector(".lineBurger");
 var btnClose = document.querySelector(".closeX");
 var sideBar = document.querySelector(".sideBar");
 var body = document.querySelector(".body");
+var animaContato = document.querySelector(".animationContato");
+var btnContato = document.getElementById("contato-sideBar");
+
+btnContato.addEventListener("click", () => {
+  if (animaContato.getAttribute("open") == "false") {
+    animaContato.setAttribute("open", "open");
+    animaContato.style.transition = ".5s";
+    animaContato.style.height = "130px";
+  } else {
+    animaContato.setAttribute("open", "false");
+    animaContato.style.transition = ".5s";
+    animaContato.style.height = "0px";
+  }
+});
 
 btnBurger.addEventListener("click", () => {
   body.style.overflow = "hidden";
@@ -12,4 +26,5 @@ btnClose.addEventListener("click", () => {
   sideBar.style.transition = "0.3s";
   sideBar.style.width = "0vw";
   body.style.overflow = "visible";
+  animaContato.style.height = "0px";
 });
